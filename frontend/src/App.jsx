c8cb552ad99f843adde07dcc18fc0369216a37e1
angelmarte <robotking134@gmail.com>
2024-07-23 12:29:02 -0400

    techwithTim full Crud

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import ContactForm from './ContactForm'
+import ContactList from './ContactList'
+
+function App() {
+const [contacts,setContacts] = useState([])
+
+useEffect(()=> {
+  fetchContacts()
+},[])
+
+const fetchContacts = async () => {
+  const response = await fetch("http://127.0.0.1:5000/contacts")
+  const data = await response.json()
+  setContacts(data.contacts)
+  console.log(data.contacts)
+}
+
+  return <>
+  <ContactList contacts={contacts}/>
+  <ContactForm/>
+  </>
+}
+
+export default App
